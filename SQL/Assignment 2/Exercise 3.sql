e8216b50cadb1eb5265d339dc6e88e4f1cfa140e
08_LuongKhanhToan <luongkhanhtoan8899@gmail.com>
2023-08-02 21:31:02 +0700

    updated

--- SQL/Assignment 2/Exercise 3.sql
+++ SQL/Assignment 2/Exercise 3.sql
@@ -3,5 +3,5 @@ CREATE TABLE `fresher.exercise3` (
     Ten VARCHAR(30) NOT NULL,
     BirthDate DATE NOT NULL,
     Gender ENUM('0', '1', 'unknown') NOT NULL,
-    IsDeletedFlag BOOL CHECK( IsDeletedFlag=0 OR IsDeletedFlag=1)
+    IsDeletedFlag ENUM('0', '1')
 );
