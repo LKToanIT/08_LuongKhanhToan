2d4340039aa27e1a108a9425e3df5df5b2d565ca
08_LuongKhanhToan <luongkhanhtoan8899@gmail.com>
2023-08-02 20:00:31 +0700

    Assignment

--- SQL/Assignment 2/Exercise 3.sql
+++ SQL/Assignment 2/Exercise 3.sql
@@ -3,5 +3,5 @@ CREATE TABLE fresher.exercise3 (
     ten VARCHAR(30),
     BirthDate DATE,
     Gender ENUM('male', 'famale', 'unknown'),
-    IsDeletedFlag BOOL
+    IsDeletedFlag bool check( IsDeletedFlag=0 or IsDeletedFlag=1)
 );
